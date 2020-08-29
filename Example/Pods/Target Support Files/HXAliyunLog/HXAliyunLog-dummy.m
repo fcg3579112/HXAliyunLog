b9beeafb9a9e40fdac13438b590bb970245adcf1
冯传高 <fengchuangao@huixuanjiasu.com>
2020-08-29 09:54:32 +0800

    Feat: 编辑阿里云pod

---
+++ Example/Pods/Target Support Files/HXAliyunLog/HXAliyunLog-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HXAliyunLog : NSObject
+@end
+@implementation PodsDummy_HXAliyunLog
+@end
---
+++ Example/Pods/Target Support Files/Pods-HXAliyunLog_Tests/Pods-HXAliyunLog_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HXAliyunLog_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_HXAliyunLog_Tests
+@end
